afc405aff3c41afff80089f442aa4aac1f2a7f3b
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-12-16 18:55:20 +0100

    mongodb conn and  user and event model

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -30,6 +30,10 @@ export default function Home() {
       </section>
       <section className="wrapper my-8 flex flex-col gap-8 md:gap-12" id="events">
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search
+          CatFilter
+        </div>
       </section>
     </>
   );
