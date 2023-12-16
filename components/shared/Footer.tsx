e7e8f249a7683322cd66097f0ee665423e53fadb
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-12-16 18:25:00 +0100

    Footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href={"/"}>
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
+        <p>2023 Evenly. All Rights reserved</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
