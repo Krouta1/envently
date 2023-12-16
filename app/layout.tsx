66ec4bed15329e8890e227ed1b4efd2a0cdadc1e
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2023-12-16 18:02:38 +0100

    header and auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -9,12 +10,12 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: "Evently",
+  description: "Evently is a platform for event management.",
   icons: {
-    icon: '/assets/images/logo.svg'
-  }
-}
+    icon: "/assets/images/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -22,8 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
