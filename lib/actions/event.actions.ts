d6d97282e7b41701e83fa3f5108c733a2c6561ef
Petr Kroutil <pkroutil1@seznam.cz>
2023-12-20 12:13:09 +0100

    collection on home page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { CreateEventParams } from "@/types";
+import { CreateEventParams, GetAllEventsParams } from "@/types";
 import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
@@ -47,6 +47,32 @@ export const getEventById = async (eventId: string) => {
 
     if (!event) throw new Error("Event not found!");
     return JSON.parse(JSON.stringify(event));
+
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getAllEvents = async ({query, limit=6,page, category}:GetAllEventsParams) => {
+  try {
+    await connectToDatabase();
+
+    const conditions = {}
+
+    const eventsQuery = Event.find(conditions)
+      .sort({createdAt:"desc"})
+      .skip(0)
+      .limit(limit)
+    
+    const events = await populateEvent(eventsQuery)
+
+    //total number of events
+    const eventsCount = await Event.countDocuments(conditions)
+
+    return{
+      data: JSON.parse(JSON.stringify(events)),
+      totalPages: Math.ceil(eventsCount / limit)
+    }
 
   } catch (error) {
     handleError(error);
